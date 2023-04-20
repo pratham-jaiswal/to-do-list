be2be6484009827c9ed770cc0342d3dfb72aa6f5
pratham-jaiswal <prathamj0502@gmail.com>
2023-04-21 02:17:24 +0530

    Add work route

--- app.js
+++ app.js
@@ -6,21 +6,31 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let items = ['Drink Water', 'Go for a run']
+let tasks = ['Drink Water', 'Go for a run'];
+let workTasks = [];
 
 app.get("/", function(req, res){
     let today = new Date();
     const day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let displayDate = day[today.getDay()] +", "+ month[today.getMonth()]+" "+today.getDate();
-    res.render("list", {date: displayDate, newListItems: items});
+    res.render("list", {listTitle: displayDate, newListTasks: tasks, route:"/"});
 });
 
 app.post("/", function(req, res){
-    let item = req.body.newItem.trim();
-    if (item) items.push(item);
-    items.push();
+    let task = req.body.newItem.trim();
+    if (task) tasks.push(task);
     res.redirect("/");
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListTasks: workTasks, route:"/work"});
+});
+
+app.post("/work", function(req, res){
+    let task = req.body.newItem.trim();
+    if (task) workTasks.push(task);
+    res.redirect("/work");
 });
 
 app.listen(3000, function(){
